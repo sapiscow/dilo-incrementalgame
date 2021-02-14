f97c7a55194e8cdfe9789bc699bf8017157649ce
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2021-02-15 00:03:33 +0700

    Add Instance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager> ();
+            }
+
+            return _instance;
+        }
+    }
+
     // Fungsi [Range (min, max)] ialah menjaga value agar tetap berada di antara min dan max-nya
     [Range (0f, 1f)]
     public float AutoCollectPercentage = 0.1f;
